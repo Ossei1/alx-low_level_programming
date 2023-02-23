8a0ac73b59d58e05404bec1589dc2c5fec538e56
anthonyowusu <anthonyowusu5@gmail.com>
2023-02-23 10:49:48 -0800

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,15 +2,13 @@
 
 /**
  * print_numbers - prints 0123456789\n
- * @a: the character to check
- * Return: void
+ * Return: the number 0-9
  */
 
 void print_numbers(void)
-
 {
 	int a;
-
+	
 	for (a = '0'; a <= '9'; a++)
 	{
 	_putchar(a);
