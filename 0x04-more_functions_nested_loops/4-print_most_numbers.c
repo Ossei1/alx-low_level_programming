e02d5ab7244c64d01dd5de05069acd95343014ad
anthonyowusu <anthonyowusu5@gmail.com>
2023-02-23 11:20:01 -0800

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,14 +12,10 @@ void print_most_numbers(void)
 
 	for (x = 0; x <= 9; x++)
 	{
-	if (x == 2 || x == 4)
+	if (x != 2 && x != 4)
 	{
-	continue;
-	}
-	else
-	{
-	_putchar(x + '0');
+	_putchar(x);
 	}
 	}
-	_putchar('\n')
+	_putchar('\n');
 }
