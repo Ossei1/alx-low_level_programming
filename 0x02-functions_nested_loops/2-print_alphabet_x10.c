833c7cfc5983960abc9799d7f3e77d85d13602de
Ossei1 <anthonyowusu5@gmail.com>
2023-06-07 08:11:48 -0700

    Functions

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ *
+ * Return: always 0;
+ *
+ */
+void print_alphabet_x10(void)
+{
+	char j;
+	char h;
+
+	for (h = 0; h <= 9; h++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
